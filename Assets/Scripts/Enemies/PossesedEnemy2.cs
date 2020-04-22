9085c1d6f6c01999a2e62c77af06b97f5fb4b277
Aitor <aitrioant@gmail.com>
2020-04-22 12:26:14 +0200

    Prefab ready

--- Assets/Scripts/Enemies/PossesedEnemy2.cs
+++ Assets/Scripts/Enemies/PossesedEnemy2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class PossesedEnemy2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
