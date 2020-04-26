2969045edf21fa2fddd0321ad37bbf7f4cc1ddee
HaruKawai <angelginer27@gmail.com>
2020-04-26 20:42:07 +0200

    Cambio de niveles

---
+++ Assets/Scripts/CambioDeNivel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioDeNivel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player") 
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 3) SceneManager.LoadScene(0);
+            else SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
